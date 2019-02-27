4c4e95ffe51f33cd9dd93147bc875bcc44fc1884
Puja Grover <puja_grover@yahoo.com>
2019-02-26 23:37:22 -0600

    Class5HwRandomMax. Commit, Push, Sync

--- RandomMax/Program.cs
+++ RandomMax/Program.cs
@@ -8,6 +8,20 @@ namespace RandomMax
         {
             // TODO: Generate a list of 100 random integers between 0 and 1000 and ouput the maximum value.
 
+            int randomMax = 0;
+
+            for (int i = 1; i < 101; i++)
+            {
+                int randomInteger = new Random().Next(0, 1000);
+                //Console.WriteLine($"{i})" + randomInteger);
+
+                if (randomInteger > randomMax)
+                {
+                    randomMax = randomInteger;
+                }
+            }
+            Console.WriteLine(randomMax);
+
             Console.ReadLine();
         }
     }
